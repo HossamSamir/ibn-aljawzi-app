eae2af0b629048a5f78a1a38172fde0d0f6a3ebb
Hossam Samir <Hossameos1@gmail.com>
2017-11-21 21:29:28 +0200

    header improvments and new logo

--- components/StyledText.js
+++ components/StyledText.js
@@ -18,7 +18,7 @@ import { Ionicons } from '@expo/vector-icons';
 export default class Header extends React.Component {
   render() {
     return (
-        <View style={{ flex: .1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center',  backgroundColor: '#106234', paddingVertical: 5, paddingHorizontal: 15, paddingTop: Constants.statusBarHeight }}>
+        <View style={{ flex: .1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center',  backgroundColor: '#106234', paddingTop: Platform.OS == 'ios' ? 20 : 0, paddingVertical: 5, paddingHorizontal: 15, }}>
             <View style={{ flex: .5, paddingRight: 50}}>
             <Ionicons
               name='ios-menu-outline'
@@ -37,12 +37,18 @@ export default class Header extends React.Component {
             />
 
             </View>
-            <View style={{ flex: 3, }}>
+            <View style={{ flex: 3, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginRight: 15 }}>
                 <TextInput
                     underlineColorAndroid='transparent'
                     placeholder='seacrh'
                     placeholderTextColor='rgba(16, 98, 52, 0.5803921568627451)'
-                    style={{ backgroundColor: 'white', borderRadius: 10, fontSize: 20, color: '#106234', padding: 5,  paddingLeft: 20 }}
+                    style={{ flex: 1, backgroundColor: 'white', borderRadius: 10, fontSize: 18, color: '#106234', padding: 1,  paddingLeft: 20 }}
+                />
+                <Ionicons
+                  name='ios-search-outline'
+                  size={23}
+                  color='rgba(16, 98, 52, 0.5803921568627451)'
+                  style={{ marginLeft: -30, backgroundColor: 'transparent' }}
                 />
             </View>
         </View>
