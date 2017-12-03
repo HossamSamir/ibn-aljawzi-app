4d0569a847af1a9d10062090d06c5eb83eb0fa3f
Khalid <khalidahmed333@hotmail.com>
2017-12-03 19:56:09 +0200

    Add sign-up & sign-in screens, closes #2
    Only UI.

---
+++ screens/menu/Signup.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import { StyleSheet, TextInput, View, Text, Image } from "react-native";
+import { Button } from "react-native-elements";
+import { Ionicons } from '@expo/vector-icons';
+
+export default class Signup extends React.Component {
+    static navigationOptions = {
+        title: "Sign up"
+    };
+
+    render() {
+        return (
+            <View style={{ backgroundColor: 'white', height: '100%', flex: 1, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', }}>
+                <Image
+                  style={{
+                      resizeMode: 'cover',
+                      width: '100%',
+                      height: '40%',
+                      marginBottom: 30
+                  }}
+                  source={require('../../assets/images/register_cover.png')}/>
+
+                <View style={styles.inputsContainer}>
+                    <View style={styles.singleInputContainer}>
+                        <Ionicons
+                          name={'md-contact'}
+                          size={26}
+                          color='#63BA83'
+                          style={styles.inputIcon}/>
+
+                          <TextInput
+                              underlineColorAndroid='transparent'
+                              placeholder='Username'
+                              placeholderTextColor='#BBBBBB'
+                              autoGrow={false}
+                              multiline={false}
+                              autoFocus={false}
+                              style={styles.textInput}
+                          />
+                    </View>
+
+                    <View style={styles.singleInputContainer}>
+                        <Ionicons
+                          name={'md-lock'}
+                          size={26}
+                          color='#63BA83'
+                          style={styles.inputIcon}/>
+
+                          <TextInput
+                              underlineColorAndroid='transparent'
+                              placeholder='Password'
+                              placeholderTextColor='#BBBBBB'
+                              autoGrow={false}
+                              multiline={false}
+                              autoFocus={false}
+                              secureTextEntry={true}
+                              style={styles.textInput}
+                          />
+                    </View>
+
+                    <View style={styles.singleInputContainer}>
+                        <Ionicons
+                          name={'md-lock'}
+                          size={26}
+                          color='#63BA83'
+                          style={styles.inputIcon}/>
+
+                          <TextInput
+                              underlineColorAndroid='transparent'
+                              placeholder='Confirm password'
+                              placeholderTextColor='#BBBBBB'
+                              autoGrow={false}
+                              multiline={false}
+                              autoFocus={false}
+                              secureTextEntry={true}
+                              style={styles.textInput}
+                          />
+                    </View>
+
+                    <View style={styles.buttonContainer}>
+                        <Button
+                            onPress={() => this.props.navigation.navigate("NONE")}
+                            color='white'
+                            backgroundColor='#106234'
+                            containerViewStyle={{borderRadius:20}}
+                            borderRadius={20}
+                            fontWeight='bold'
+                            buttonStyle={{width: '100%', padding: 9}}
+                            title="Sign up" />
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    textInput: {
+        flex: 1,
+        color: '#106234'
+    },
+    inputIcon: {
+        backgroundColor: 'transparent',
+        marginRight: 9
+    },
+    inputsContainer: {
+        flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center',
+        width: '75%'
+    },
+    singleInputContainer: {
+        flex: 0.2,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        borderBottomColor: '#63BA83',
+        borderBottomWidth: 0.5
+    },
+    buttonContainer: {
+        marginTop: 35,
+        flex: 0.4
+    }
+});
