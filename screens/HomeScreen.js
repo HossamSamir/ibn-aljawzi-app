9ed35450d651464462cb74202758f0d221401ef3
Khalid <khalidahmed333@hotmail.com>
2017-12-07 01:24:50 +0200

    Fetching of Home screen, addresses #15

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,21 +15,44 @@ import { Rating } from 'react-native-elements';
 
 //import Header from '../components/Header';
 import OneBookCard from '../components/OneBookCard';
+import LoadingIndicator from '../components/LoadingIndicator';
 
 export default class HomeScreen extends React.Component {
+    componentDidMount() {
+        this.doTheFetching();
+    }
+
+    doTheFetching() {
+        fetch('https://ca235020.ngrok.io/api/categories').then((res) => res.json()).then((resJson) => {
+            this.setState({mainCats: resJson});
+        })
+        .then(() => {
+          this.setState({doneFetches: (this.state.doneFetches+1)})
+        })
+
+        fetch('https://ca235020.ngrok.io/api/homescreen').then((res) => res.json()).then((resJson) => {
+            this.setState({booksInCats: resJson});
+        })
+        .then(() => {
+          this.setState({doneFetches: (this.state.doneFetches+1)})
+        })
+
+
+    }
 
     constructor(props) {
         super(props);
         this.state = {
+            doneFetches: 0,
             mainCats: [
-                {cat_ID: 1, cat_name: 'Top selling'},
-                {cat_ID: 1, cat_name: 'Trending'},
-                {cat_ID: 2, cat_name: 'Soulful'},
-                {cat_ID: 3, cat_name: 'History'},
-                {cat_ID: 4, cat_name: 'Entertainment'},
+                /*{id: 0, name: 'Top selling'},
+                {id: 1, name: 'Trending'},
+                {id: 2, name: 'Soulful'},
+                {id: 3, name: 'History'},
+                {id: 4, name: 'Entertainment'},*/
             ],
             booksInCats: [
-                {
+                /*{
                     cat_ID: 1, cat_name: "Top selling",
                     cat_books: [
                         {book_ID: 1, book_name: 'Book name', book_photo: 'https://orig00.deviantart.net/9da8/f/2010/332/8/5/islamic_book_cover_by_sherif_designer-d33s4kd.jpg',  author_name: "Ahmed Hassan"},
@@ -88,10 +111,12 @@ export default class HomeScreen extends React.Component {
                         {book_ID: 2, book_name: 'Book name', book_photo: 'https://orig00.deviantart.net/9da8/f/2010/332/8/5/islamic_book_cover_by_sherif_designer-d33s4kd.jpg',  author_name: "Ahmed Hassan"},
                         {book_ID: 7, book_name: 'Book name', book_photo: 'https://orig00.deviantart.net/9da8/f/2010/332/8/5/islamic_book_cover_by_sherif_designer-d33s4kd.jpg',  author_name: "Ahmed Hassan"},
                     ]
-                },
-                ],
+                },*/
+            ],
         }
     }
+
+_keyExtractor0 = (item, index) => item.id;
 _keyExtractor = (item, index) => item.cat_ID;
 _keyExtractor2 = (item, index) => item.book_ID;
 
@@ -101,6 +126,9 @@ _keyExtractor2 = (item, index) => item.book_ID;
   //navigationOptions = (navigation) => {header: <Header nav={navigation} />};
 
   render() {
+      if(this.state.doneFetches < 2)
+          return (<LoadingIndicator size="large" color="#B6E3C6" />);
+
     return (
       <ScrollView style={styles.container}>
       <ImageBackground
@@ -114,12 +142,12 @@ _keyExtractor2 = (item, index) => item.book_ID;
         showsHorizontalScrollIndicator={false}
         style={{ maxHeight: 80, }}
         data = {this.state.mainCats}
-        keyExtractor={this._keyExtractor}
+        keyExtractor={this._keyExtractor0}
         renderItem = {({ item }) => (
             <TouchableOpacity onPress={ () => {
-              this.props.navigation.navigate('Category', {main_cat_id: item.cat_ID, cat_name: item.cat_name})
+              this.props.navigation.navigate('Category', {main_cat_id: item.id, cat_name: item.name})
             }}>
-                <Text style={{ color: 'white', backgroundColor: '#106234', paddingVertical: 10, paddingHorizontal: 40, margin: 20, borderRadius: 18, fontSize: 12, fontWeight: 'bold' }}>{item.cat_name.toUpperCase()}</Text>
+                <Text style={{ color: 'white', backgroundColor: '#106234', paddingVertical: 10, paddingHorizontal: 40, margin: 20, borderRadius: 18, fontSize: 12, fontWeight: 'bold' }}>{item.name.toUpperCase()}</Text>
             </TouchableOpacity>
         )} />
 
