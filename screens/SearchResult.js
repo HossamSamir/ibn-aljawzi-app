c5f8cfcd9454955346d12e60004a3581dd82059b
Khalid <khalidahmed333@hotmail.com>
2017-12-12 18:09:29 +0200

    Fixes #39 and fixes #40

--- screens/SearchResult.js
+++ screens/SearchResult.js
@@ -92,7 +92,7 @@ export default class MyLibrary extends React.Component {
                                 <TouchableOpacity onPress={ () => {
                                   this.props.navigation.navigate('Book', {book_ID: item.id, book_photo: item.book_photo, book_name: item.book_name, author_name: item.author_name})
                                 }}>
-                                    <OneBookCard navigation={this.props.navigation}  id={item.id} addButton={1} book_name={item.book_name} book_photo={item.book_photo} author_name={item.author_name} />
+                                    <OneBookCard navigation={this.props.navigation}  id={item.id} horizontal={0} addButton={1} book_name={item.book_name} book_photo={item.book_photo} author_name={item.author_name} />
                                 </TouchableOpacity>
                             </View>
                         )} />
