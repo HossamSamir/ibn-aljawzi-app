038438d22921f20d4d9b83e2c192870bf4c19d4f
Khalid <khalidahmed333@hotmail.com>
2018-01-12 09:02:51 +0200

    Add About us text

--- screens/menu/AboutUs.js
+++ screens/menu/AboutUs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from "react-native";
+import { ScrollView, Text } from "react-native";
 
 import MenuBackButton from './MenuBackButton'
 
@@ -10,11 +10,29 @@ export default class AboutUs extends React.Component {
 
     render() {
         return (
-            <View style={{ backgroundColor: '#FCFFFD', height: '100%' }}>
+            <ScrollView style={{ backgroundColor: '#FCFFFD', height: '100%' }}>
                 <MenuBackButton navigation={this.props.navigation} />
-                <Text style={{ color: '#0E142A', backgroundColor: 'transparent', fontWeight: 'bold', fontSize: 18, margin: 12, marginTop: 20}}>About Ibn Al-Jawzi</Text>
-                <Text style={{ color: '#126330', backgroundColor: '#F5FAF7', fontSize: 16, padding: 12, marginTop: 10}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.</Text>
-            </View>
+                <Text style={{ color: '#0E142A', backgroundColor: 'transparent', fontWeight: 'bold', fontSize: 18, margin: 12, marginTop: 20}}>عن دار ابن الجوزي</Text>
+                <Text style={{ color: '#126330', backgroundColor: '#F5FAF7', fontSize: 16, padding: 12, marginTop: 10}}>{`
+الحمد لله والصلاة والسلام على رسول الله وعلى آله وصحبه أجمعين، أما بعد :
+
+يسرنا أخي الزائر الكريم أن نقدم لك نبذة مختصرة عن دار ابن الجوزي للنشر والتوزيع.
+تـأسـسـت الـدار عـام خـمـس وأربـعمائـة وألـف مـن هـجـرة الـمـصطـفى صـلى الله عـلـيه وسـلـم وهي تهدف إلى:
+•	نشر العلم وإشاعة المعرفة وبث الوعي والإسهام في نشر وإحياء التراث الإسلامي.
+•	السعي لإحياء التراث الإسلامي وتقريب العلوم الإسلامية وتيسير الاستفادة منها.
+•	نشر نفائس الكتب والمراجع الإسلامية وإخراج الجديد من كتب أهل العلم والفضل والتحـقيق من التراث العلمي لسلفنا الصالح ومن سار على دربهم.
+•	نشر الرسائل الجامعية النافعة والمفيدة.
+•	تقديم المنهج الإسلامي الصحيح والنقي من الشوائب في الاعتقاد والعبادة والمعاملات، وتطهير وتزكية النفوس والقلوب وإحياء المعاني الإيمانية.
+•	العناية بالمادة العلمية والتحقيق العلمي.
+•	الإخراج الفني الراقي الذي يليق بالكتاب الإسلامي.
+•	احترام وتقدير لحقوق التأليف والابتكار وعدم الاعتداء عليها.
+•	المشاركة في المعارض الدولية والمحلية المتخصصة .
+
+ونحمد الله عز وجل ونشكره لتوفيقه لنا لهذا العمل وعلى ما لمسناه من أهل العلم من تشجيع ونصح وإرشاد وبارك الله في جهود الجميع  لخدمة دينه وتقريبه للناس كافة.
+
+مع تمنياتنا للجميع بالعلم النافع والعمل الصالح
+                `}</Text>
+            </ScrollView>
         );
     }
 }
