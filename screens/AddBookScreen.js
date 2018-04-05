f288dc5525f6f3e269595654e0e84e8eac92b6ab
ZizoHussien <36141034+ZizoHussien@users.noreply.github.com>
2018-04-05 23:12:04 +0200

    Add fetching code to recent-books screen

--- screens/AddBookScreen.js
+++ screens/AddBookScreen.js
@@ -20,55 +20,43 @@ import OneBookCard from '../components/OneBookCard';
 import Server from '../constants/server';
 
 export default class AddBook extends React.Component {
-
+    doTheFetching() {
+        fetch(Server.dest + '/api/new_books', {headers: {'Cache-Control': 'no-cache'}}).then((res) => res.json()).then((resJson) => {
+            this.setState({books: resJson});
+        })
+        .then(() => {
+          this.setState({doneFetching: true})
+        })
+    }
     constructor(props)
         {
           super(props);
-          this.state = { books: [
-                  {id: 0, book_name: 'Book1 name', book_photo: 'https://orig00.deviantart.net/9da8/f/2010/332/8/5/islamic_book_cover_by_sherif_designer-d33s4kd.jpg',  author_name: "Ahmed Hassan"},
-                  {id: 1, book_name: 'Book2 name', book_photo: 'https://orig00.deviantart.net/9da8/f/2010/332/8/5/islamic_book_cover_by_sherif_designer-d33s4kd.jpg',  author_name: "Ahmed Hassan"},
-                  {id: 2, book_name: 'Book3 name', book_photo: 'https://orig00.deviantart.net/9da8/f/2010/332/8/5/islamic_book_cover_by_sherif_designer-d33s4kd.jpg',  author_name: "Ahmed Hassan"},
-                  {id: 3, book_name: 'Book4 name', book_photo: 'https://orig00.deviantart.net/9da8/f/2010/332/8/5/islamic_book_cover_by_sherif_designer-d33s4kd.jpg',  author_name: "Ahmed Hassan"},
-                  {id: 4, book_name: 'Book5 name', book_photo: 'https://orig00.deviantart.net/9da8/f/2010/332/8/5/islamic_book_cover_by_sherif_designer-d33s4kd.jpg',  author_name: "Ahmed Hassan"},
-                  {id: 5, book_name: 'Book6 name', book_photo: 'https://orig00.deviantart.net/9da8/f/2010/332/8/5/islamic_book_cover_by_sherif_designer-d33s4kd.jpg',  author_name: "Ahmed Hassan"},
-                  {id: 6, book_name: 'Book7 name', book_photo: 'https://orig00.deviantart.net/9da8/f/2010/332/8/5/islamic_book_cover_by_sherif_designer-d33s4kd.jpg',  author_name: "Ahmed Hassan"},
-                  {id: 7, book_name: 'Book8 name', book_photo: 'https://orig00.deviantart.net/9da8/f/2010/332/8/5/islamic_book_cover_by_sherif_designer-d33s4kd.jpg',  author_name: "Ahmed Hassan"},
-                  {id: 8, book_name: 'Book9 name', book_photo: 'https://orig00.deviantart.net/9da8/f/2010/332/8/5/islamic_book_cover_by_sherif_designer-d33s4kd.jpg',  author_name: "Ahmed Hassan"},
-                  {id: 9, book_name: 'Book10 name', book_photo: 'https://orig00.deviantart.net/9da8/f/2010/332/8/5/islamic_book_cover_by_sherif_designer-d33s4kd.jpg',  author_name: "Ahmed Hassan"},
-                  {id: 10, book_name: 'Book11 name', book_photo: 'https://orig00.deviantart.net/9da8/f/2010/332/8/5/islamic_book_cover_by_sherif_designer-d33s4kd.jpg',  author_name: "Ahmed Hassan"},
-                  {id: 11, book_name: 'Book12 name', book_photo: 'https://orig00.deviantart.net/9da8/f/2010/332/8/5/islamic_book_cover_by_sherif_designer-d33s4kd.jpg',  author_name: "Ahmed Hassan"},
-              ],
-              subCats: [
-                  {id: 0, cat_name: 'Sub category 1'},
-                  {id: 1, cat_name: 'Sub category 2'},
-                  {id: 2, cat_name: 'Sub category 3'},
-                  {id: 3, cat_name: 'Sub category 4'},
-                  {id: 4, cat_name: 'Sub category 5'},
-              ],
+          this.state = {
+              doneFetching:false,
+              books: [],
               thingsToTranslate:{NewBooks:'NewBooks'}
           }
         }
         componentDidMount ()
         {
-
             AsyncStorage.getItem("language").then((value) => {
               if (value == '1') {
                 this.setState({ thingsToTranslate: {  NewBooks:'الكتب الجديدة' } })
               } else {
                 this.setState({ thingsToTranslate: {  NewBooks:'New Books' } })
               }
+              this.doTheFetching();
             });
         }
-
-
   render() {
 
     return (
         <View>
+        <View style={{borderBottomWidth:0.7,borderBottomColor:'#555555',paddingTop:10}}>
         <Text style={{ marginHorizontal: 20, fontWeight: 'bold', color: '#555555', fontSize: 20, borderBottomWidth: 1, borderColor: 'grey', paddingBottom: 10 }}>
         {this.state.thingsToTranslate.NewBooks}
         </Text>
-
+        </View>
           <FlatList
             style={{ flexDirection: 'column' }}
             contentContainerStyle={{ alignItems: 'center' ,}}
