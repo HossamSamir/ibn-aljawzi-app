6a680b9b1fc7911e3aac357f5d80ac19719b6d2e
ZizoHussien <36141034+ZizoHussien@users.noreply.github.com>
2018-04-16 17:36:56 +0200

    Add maps markers data, Close #64

--- components/MapTab.js
+++ components/MapTab.js
@@ -13,8 +13,6 @@ export default class MapTab extends React.Component {
              latitudeDelta: 0.0922,
              longitudeDelta: 0.0421,
            }}>
-
-
         <MapView.Marker
             coordinate={{
                 latitude: 24.824744,
@@ -24,7 +22,6 @@ export default class MapTab extends React.Component {
             description="فرع رقم واحد"
             onPress={() => Linking.openURL('maps://app?saddr=Current+Location&daddr=24.824744,46.615029')}
         />
-
         <MapView.Marker
             coordinate={{
                 latitude: 24.845844,
@@ -33,8 +30,42 @@ export default class MapTab extends React.Component {
             title="دار ابن الجوزي للنشر والتوزيع المملكة العربية السعودية"
             description="فرع رقم اثنان"
             onPress={() => Linking.openURL('maps://app?saddr=Current+Location&daddr=24.845844,46.616029')}
-
-
+        />
+        <MapView.Marker
+            coordinate={{
+                latitude: 26.425453,
+                longitude: 50.09854169999994,
+            }}
+            title="دار ابن الجوزي للنشر والتوزيع المملكة العربية السعودية"
+            description="فرع رقم ثلاثة"
+            onPress={() => Linking.openURL('maps://app?saddr=Current+Location&daddr=26.425453,50.09854169999994')}
+        />
+        <MapView.Marker
+            coordinate={{
+                latitude: 24.8030267,
+                longitude: 46.70358149999993,
+            }}
+            title="دار ابن الجوزي للنشر والتوزيع المملكة العربية السعودية"
+            description="فرع رقم اربعة"
+            onPress={() => Linking.openURL('maps://app?saddr=Current+Location&daddr=24.8030267,46.70358149999993')}
+        />
+        <MapView.Marker
+            coordinate={{
+                latitude: 21.4805137,
+                longitude: 39.23751400000003,
+            }}
+            title="دار ابن الجوزي للنشر والتوزيع المملكة العربية السعودية"
+            description="فرع رقم خمسة"
+            onPress={() => Linking.openURL('maps://app?saddr=Current+Location&daddr=21.4805137,39.23751400000003')}
+        />
+        <MapView.Marker
+            coordinate={{
+                latitude: 25.3631681,
+                longitude: 49.5882304,
+            }}
+            title="دار ابن الجوزي للنشر والتوزيع المملكة العربية السعودية"
+            description="فرع رقم ستة"
+            onPress={() => Linking.openURL('maps://app?saddr=Current+Location&daddr=25.3631681,49.5882304')}
         />
         </MapView>
     );
